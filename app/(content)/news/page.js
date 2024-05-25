94443b26968760954a7d597897b59b2fe8a9ec4a
brianjsyang <brianjsyang@gmail.com>
2024-05-24 20:53:34 -0700

    Fetch data from a seperate backend via API call

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,12 +1,20 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 
-export default function NewsPage() {
-    return (
-      <div>
-        <h1>News Page</h1>
-        <NewsList news={DUMMY_NEWS} />
-      </div>
-    );
+export default async function NewsPage() {
+  // fetch the news from the backend via API
+  const response = await fetch('http://localhost:8080/news');
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch news');
   }
+
+  const news = await response.json();
+  
+  return (
+    <>
+      <h1>News Page</h1>
+      <NewsList news={news} />
+    </>
+  );
+}
   
